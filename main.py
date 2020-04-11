aa83f8771e5302281d84417d6d8ac51c63931353
Dawid Paluszkiewicz <dawidpaluszkiewicz.00@gmail.com>
2020-04-11 18:17:58 +0200

    add pdf parser

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 from helpers import *
 
+
 def main():
 
     files = get_all_pdf_files(path)
 
+    file_text = []
+    for file in files:
+        f = os.path.join(path, file)
+        text = convert_pdf_to_txt(f)
+        file_text.append((file, text))
 
 if __name__ == '__main__':
     main()
